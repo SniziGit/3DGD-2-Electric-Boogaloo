50c922b1b57a1a3e7bb035235178beb464eee108
SniziGit <189128179+SniziGit@users.noreply.github.com>
2026-02-09 00:45:29 +0800

    movement tweak

--- Assets/Scripts/FPSMovement.cs
+++ Assets/Scripts/FPSMovement.cs
@@ -3,17 +3,45 @@ using UnityEngine.InputSystem;
 
 public class FPSMovement : MonoBehaviour
 {
-    public float speed = 5f;
-    private Vector2 moveInput;
+    PlayerInput playerInput;
+    InputAction moveAction, jumpAction, lookAction;
 
+    Rigidbody rb;
+
+    [SerializeField] float speed = 5f;
     void OnMove(InputValue value)
     {
-        moveInput = value.Get<Vector2>();
+        playerInput = GetComponent<PlayerInput>();
+        moveAction = playerInput.actions.FindAction("Move");
+        jumpAction = playerInput.actions.FindAction("Jump");
+        lookAction = playerInput.actions.FindAction("Look");
     }
 
     void Update()
     {
-        Vector3 move = new Vector3(moveInput.x, 0, moveInput.y);
-        transform.Translate(move * speed * Time.deltaTime, Space.World);
+        MovePlayer();
+    }
+
+    private void OnEnable()
+    {
+        jumpAction.performed += JumpPlayer;
+    }
+
+    private void OnDisable()
+    {
+        jumpAction.performed -= JumpPlayer;
+    }
+
+    void MovePlayer()
+    {
+        Vector3 val = moveAction.ReadValue<Vector2>();
+        Vector3 direction = new Vector3(val.x, 0, val.y);
+
+        rb.linearVelocity = direction * speed * Time.deltaTime;
+    }
+    void JumpPlayer(InputAction.CallbackContext context)
+    {
+        //if (jumpAction.triggered)
+            //Vector2 jumpHeight = jumpAction.ReadValue<Vector2>();
     }
 }
