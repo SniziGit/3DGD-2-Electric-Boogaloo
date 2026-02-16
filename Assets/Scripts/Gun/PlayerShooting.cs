e4e8ba8aca757c2fe80413799c98b3be586ccde8
SniziGit <189128179+SniziGit@users.noreply.github.com>
2026-02-16 18:39:24 +0800

    Canvas Prefab and ADS added

--- Assets/Scripts/Gun/PlayerShooting.cs
+++ Assets/Scripts/Gun/PlayerShooting.cs
@@ -5,15 +5,26 @@ public class PlayerShooting : MonoBehaviour
 {
     public Gun gun;
     private bool isHoldingShoot = false;
+    private bool isAiming = false;
 
     void OnShoot()
     {
         isHoldingShoot = true;
     }
 
+    void OnAim()
+    {
+        isAiming = true;
+    }
+
     void OnShootRelease()
     {
         isHoldingShoot = false;
+    }
+
+    void OnAimRelease()
+    {
+        isAiming = false;
     }
 
     void OnReload()
@@ -28,6 +39,14 @@ public class PlayerShooting : MonoBehaviour
         if (isHoldingShoot && gun != null)
         {
             gun.Shoot();
+        }
+        if (isAiming && gun != null)
+        {
+            gun.Aim();
+        }
+        else
+        {
+            gun.StopAiming();
         }
     }
 }
